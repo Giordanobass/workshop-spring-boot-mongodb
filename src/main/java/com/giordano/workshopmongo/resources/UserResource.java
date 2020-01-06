29fdc7eb07e589050824dbd0440baa6f5dabec07
Giordanobass <giordanoamaral@gmail.com>
2020-01-06 14:28:48 -0300

    Operação de instanciação da base de dados

--- src/main/java/com/giordano/workshopmongo/resources/UserResource.java
+++ src/main/java/com/giordano/workshopmongo/resources/UserResource.java
@@ -1,7 +1,5 @@
 package com.giordano.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
