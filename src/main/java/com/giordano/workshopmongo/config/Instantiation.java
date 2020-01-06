f0becef408605de70026024d505bbbb4f12f02e7
Giordanobass <giordanoamaral@gmail.com>
2020-01-06 17:46:59 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/giordano/workshopmongo/config/Instantiation.java
+++ src/main/java/com/giordano/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.giordano.workshopmongo.domain.Post;
 import com.giordano.workshopmongo.domain.User;
+import com.giordano.workshopmongo.dto.AuthorDTO;
 import com.giordano.workshopmongo.repository.PostRepository;
 import com.giordano.workshopmongo.repository.UserRepository;
 
@@ -30,16 +31,20 @@ public class Instantiation implements CommandLineRunner {
 
 		userReposiroty.deleteAll();
 		postReposiroty.deleteAll();
+		
+	
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
 
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
-				maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+			new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
-		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+	
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
 	}
 
