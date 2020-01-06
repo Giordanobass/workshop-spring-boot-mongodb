80ea4ed645e7bc6dc610c9f3deface0411071332
Giordanobass <giordanoamaral@gmail.com>
2020-01-06 18:01:25 -0300

    Referenciando os posts do usuário

--- src/main/java/com/giordano/workshopmongo/config/Instantiation.java
+++ src/main/java/com/giordano/workshopmongo/config/Instantiation.java
@@ -44,8 +44,10 @@ public class Instantiation implements CommandLineRunner {
 			new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
-	
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userReposiroty.save(maria);
 	}
 
 }
